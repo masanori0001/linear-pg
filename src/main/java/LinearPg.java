85a229764075536b6c190c50117a0e38431db500
masanori-akabane <masanori.akabane@dena.com>
2018-06-26 18:15:38 +0900

    add master

--- src/main/java/LinearPg.java
+++ src/main/java/LinearPg.java
@@ -31,25 +31,25 @@ public class LinearPg {
     }
 
     private List convertToMatrix(String v){
-        List<String> rows = Arrays.asList(v.split("|"));
+        List<String> rows = Arrays.asList(v.split("\\|"));
 
         List result = rows.stream().map((s) -> {
             String[] cols = s.split(",");
-            return Arrays.asList(cols);
+            return Arrays.stream(cols).map(x -> Double.parseDouble(x)).collect(Collectors.toList());
         }).collect(Collectors.toList());
 
         return result;
     }
 
     private List calc(List matrix){
-        int min;
+        double min;
         int col_num = ((List)matrix.get(0)).size();
         int row_num = matrix.size();
         int row = 0, column = 0;
         while(true){
 
             //column selection
-            min = Integer.MAX_VALUE;
+            min = Double.MAX_VALUE;
             for(int k = 0; k < col_num - 1; k++){
                 if(getCell(matrix, row_num - 1, k) < min){
                     min = getCell(matrix, row_num - 1, k);
@@ -59,23 +59,23 @@ public class LinearPg {
             if(min >= 0) break;
 
             //row selection
-            min = Integer.MAX_VALUE;
+            min = Double.MAX_VALUE;
             for(int k = 0; k < row_num - 1; k++){
-                int div = getCell(matrix, k, col_num - 1) / getCell(matrix, k, column);
+                double div = getCell(matrix, k, col_num - 1) / getCell(matrix, k, column);
                 if( getCell(matrix, k, column) > 0 && div < min){
                     min = div;
                     row = k;
                 }
             }
 
-            int pivot = getCell(matrix, row, column);
+            double pivot = getCell(matrix, row, column);
             for(int k = 0; k < col_num; k++){
                 setCell(matrix, row, k, getCell(matrix, row, k) / pivot);
             }
 
             for(int k = 0; k < row_num; k++){
                 if (k != row){
-                    int x = getCell(matrix, k, column);
+                    double x = getCell(matrix, k, column);
                     for(int j = 0; j < col_num; j++){
                         setCell(matrix, k, j, getCell(matrix, k, j) - x * getCell(matrix, row, j));
                     }
@@ -86,11 +86,11 @@ public class LinearPg {
         return matrix;
     }
 
-    private int getCell(List matrix, int row, int col){
-        return (int)(((List)matrix.get(row)).get(col);
+    private double getCell(List matrix, int row, int col){
+        return (double)(((List)matrix.get(row)).get(col));
     }
 
-    private void setCell(List matrix, int row, int col, int val){
+    private void setCell(List matrix, int row, int col, double val){
         ((List)matrix.get(row)).set(col, val);
     }
 }
